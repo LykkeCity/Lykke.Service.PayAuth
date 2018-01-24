eb390916e6b1d5a6b93061c41677dd1844718dca
Alexander Ushakov <alexander.n.ushakov@gmail.com>
2018-01-25 01:18:01 +0300

    LPDEV-135 Service client fixed

--- client/Lykke.Service.PayAuth.Client/IPayAuthClient.cs
+++ client/Lykke.Service.PayAuth.Client/IPayAuthClient.cs
@@ -1,6 +1,7 @@
 ﻿
 using Lykke.Service.PayAuth.Client.Models;
 using System.Threading.Tasks;
+using Lykke.Service.PayAuth.Client.Models.Employees;
 
 namespace Lykke.Service.PayAuth.Client
 {
@@ -8,5 +9,19 @@ namespace Lykke.Service.PayAuth.Client
     {
         Task RegisterAsync(RegisterRequest request);
         Task<string> VerifyAsync(VerifyRequest request);
+
+        /// <summary>
+        /// Registers an employee credentials.
+        /// </summary>
+        /// <param name="model">The registration details.</param>
+        Task RegisterAsync(RegisterModel model);
+
+        /// <summary>
+        /// Validates employee credentials.
+        /// </summary>
+        /// <param name="email">The employee email.</param>
+        /// <param name="password">The employee password.</param>
+        /// <returns>The validation result.</returns>
+        Task<ValidateResultModel> ValidateAsync(string email, string password);
     }
 }
